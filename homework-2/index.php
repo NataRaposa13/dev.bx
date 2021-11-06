8fe186c9457f3bc4b7a41e5fd14b1f5431ae36cd
Лебедева Наталья <NataRaposa@yandex.ru>
2021-11-06 17:01:14 +0200

    index.php: общая структура вывода

--- homework-2/index.php
+++ homework-2/index.php
@@ -4,3 +4,14 @@ declare(strict_types=1);
 require "./movies/movies.php";
 require "./movies/movie-function.php";
 require "functions.php";
+
+printMessage("Welcome to movie list!");
+$age = readline("Enter age: ");
+
+if (!checkAge($age))
+{
+	printMessage("Wrong age");
+	return false;
+}
+
+printSelectMoviesByAge((int)$age, $movies);
