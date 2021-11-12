1615bfc061968335164f4e69365a551bc6d79194
Лебедева Наталья <NataRaposa@yandex.ru>
2021-11-12 20:56:19 +0200

    helper-functions.php: добавила функции безопасности и форматирования текста

--- homework-6/lib/helper-functions.php
+++ homework-6/lib/helper-functions.php
@@ -5,3 +5,17 @@ function printMessage(string $message)
 	echo $message . "\n";
 }
 
+function escape(string $output): string
+{
+	return htmlspecialchars($output, ENT_QUOTES);
+}
+
+function formatMessage(string $text, int $textLength = 10): string
+{
+	if (mb_strlen($text, 'UTF-8') > $textLength)
+	{
+		$text = mb_substr($text, 0, $textLength, 'UTF-8') . "...";
+	}
+
+	return $text;
+}
