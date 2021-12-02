f1c948ef26149dafdd95a41e4f7bc992e1e28054
Лебедева Наталья <NataRaposa@yandex.ru>
2021-12-03 00:34:11 +0200

    app.php: добавлен блок с данными подключения к БД

--- homework-7/config/app.php
+++ homework-7/config/app.php
@@ -4,5 +4,6 @@ $config = [
 	'title' => 'BITFLIX',
 	'menu' => ['index' => 'Главная', 'favorites' => 'Избранное'],
 	'add' => 'добавить фильм',
-	'details' => 'подробнее'
+	'details' => 'подробнее',
+	'db' => ['host' => 'localhost', 'username' => 'php', 'password' => 'php', 'dbName' => 'dev']
 ];
