45b9f4d73fe82e66c956535f2033fab175b3b417
Лебедева Наталья <NataRaposa@yandex.ru>
2021-12-03 02:59:25 +0200

    добавлены функции подключения БД и DNS

--- homework-7/data/db.php
+++ homework-7/data/db.php
@@ -25,3 +25,28 @@ function connectToDB(array $settings): mysqli
 
 	return $database;
 }
+
+function connectPDO(array $settings): PDO
+{
+	// подключение DNS
+
+	$host = $settings['host'];
+	$db = $settings['dbName'];
+	$user = $settings['username'];
+	$password = $settings['password'];
+	$charset = 'utf8mb4';
+
+	$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+	$options = [
+		PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+		PDO::ATTR_EMULATE_PREPARES   => false
+	];
+	try {
+		$pdo = new PDO($dsn, $user, $password, $options);
+	} catch (\PDOException $e) {
+		throw new \PDOException($e->getMessage(), (int)$e->getCode());
+	}
+
+	return $pdo;
+}
