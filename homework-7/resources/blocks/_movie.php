09bc5e0abf9d7b9a968fd01c33e3d2ebc61c4dcc
Лебедева Наталья <NataRaposa@yandex.ru>
2021-12-03 21:50:59 +0200

    _movie.php: замена ключей в массиве $movie

--- homework-7/resources/blocks/_movie.php
+++ homework-7/resources/blocks/_movie.php
@@ -3,23 +3,23 @@
 ?>
 
 <div class="movie-list--item">
-	<div class="movie-list--item-image" style="background-image: url(<?="./resources/images/img-content/".$movie['id'].".jpg"?>);"></div>
+	<div class="movie-list--item-image" style="background-image: url(<?="./resources/images/img-content/".$movie['ID'].".jpg"?>);"></div>
 	<div class="movie-list--item-head">
-		<div class="movie-list--item-title"><?=formatMessage($movie['title'], 24) ?></div>
-		<div class="movie-list--item-subtitle"><?= $movie['original-title'] ?></div>
+		<div class="movie-list--item-title"><?=formatMessage($movie['TITLE'], 24) ?></div>
+		<div class="movie-list--item-subtitle"><?= $movie['ORIGINAL_TITLE'] ?></div>
 		<div class="movie-list--item-wrapper"></div>
 	</div>
-	<div class="movie-list--item-description"><?= formatMessage($movie['description'], 190) ?></div>
+	<div class="movie-list--item-description"><?= formatMessage($movie['DESCRIPTION'], 190) ?></div>
 	<div class="movie-list--item-bottom">
 		<div class="movie-list--item-time">
 			<div class="movie-list--item-time--icon"></div>
-			<?=$movie['duration'].' мин.'.' / '.date('h:i', mktime(0,$movie['duration']))?>
+			<?=$movie['DURATION'].' мин.'.' / '.date('h:i', mktime(0,$movie['DURATION']))?>
 		</div>
 		<div class="movie-list--item-genre">
-				<div><?= formatGenreList($movie['genres']) ?></div>
+				<div><?= formatGenreList($movie['GENRES']) ?></div>
 		</div>
 	</div>
 	<div class="movie-list--item-overlap">
-		<a href="<?= "details" . ".php" . "?id=" . $movie['id']?>" class="movie-list--item-overlap--more"><?= "Подробнее" ?></a>
+		<a href="<?= "details" . ".php" . "?id=" . $movie['ID']?>" class="movie-list--item-overlap--more"><?= "Подробнее" ?></a>
 	</div>
 </div>
